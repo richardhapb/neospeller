17bd252797dc7c20d8f5cf43415a9d3815911558
richardhapb <richard.hapb@icloud.com>
2025-02-02 14:45:16 -0300

    feat: beta 1 only for python

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use std::io::{self, Read};
+use std::env;
+use reqwest::blocking::Client;
+use serde_json::json;
 
 fn main() {
     let mut input = String::new();
@@ -12,10 +15,17 @@ fn main() {
     let comments = comments_to_json(&input, comment_symbol, comment_qtd, false);
     let ml_comments = comments_to_json(&input, ml_comment_symbol, ml_comment_qtd, true);
 
-    let output = format!(
+    let parsed_json = format!(
         "{{\"comments\": {}, \"ml_comments\": {}}}",
         comments, ml_comments
     );
+
+    let output = check_grammar(&parsed_json).unwrap();
+
+    if output.contains("error") {
+        println!("Error: {}", output);
+        return;
+    }
 
     print!("{}", output);
 }
@@ -66,8 +76,9 @@ fn comments_to_json(
             }
 
             if capturing && comment && line.trim().to_string().len() > comment_qtd {
+                let clean_line = line.trim().to_string();
                 // Remove the comment symbol on init and end
-                new_line.push_str(&line.trim().to_string()[comment_qtd..line.len() - comment_qtd]);
+                new_line.push_str(&clean_line[comment_qtd..clean_line.len() - comment_qtd]);
                 capturing = false;
             }
 
@@ -83,4 +94,38 @@ fn comments_to_json(
     output.push_str("}");
 
     output
+}
+
+fn check_grammar(json_data: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let openai_token = env::var("OPENAI_API_KEY")?;
+    let initial_prompt = "I will send you a JSON with comments of a Python source, check the grammar, ensure that it is straightforward, clear, and concise. You should respond with the same format, the line, and the text. You can add lines if that continues to be clear; for that, you need to insert a new element with the next line number. Give me the lines ordered by line number descending.";
+
+    let url = "https://api.openai.com/v1/chat/completions";
+    let client = Client::new();
+    
+    let res = client
+        .post(url)
+        .header("Authorization", format!("Bearer {}", openai_token))
+        .header("Content-Type", "application/json")
+        .json(&json!({
+            "model": "gpt-4o-mini-2024-07-18",
+            "messages": [
+                {
+                    "role": "system",
+                    "content": initial_prompt
+                },
+                {
+                    "role": "user",
+                    "content": json_data
+                }
+            ],
+            "max_completion_tokens": 1000,
+            "temperature": 0.5,
+            "response_format": {"type": "json_object"}
+        }))
+        .send()?;
+
+    let response_text = res.text()?;
+
+    Ok(response_text)
 }
